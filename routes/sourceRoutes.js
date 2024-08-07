1fcd704bdc22cbd0fd6505b41fc6ecf003571f32
Debayan Mondal <81500329+Debayanmondal@users.noreply.github.com>
2024-08-07 23:38:22 +0530

    Update sourceRoutes.js

--- routes/sourceRoutes.js
+++ routes/sourceRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-const sourceController = require('../controllers/sourceController');
+const sourceController = require('../controller/sourceController');
 
 // Route to get all sourceName
 router.get('/sources', sourceController.getAllSources);
