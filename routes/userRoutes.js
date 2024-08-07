4dff1f8cfb8060dc0f16cfa63d7e3a81ad230162
Debayan Mondal <81500329+Debayanmondal@users.noreply.github.com>
2024-08-07 23:43:53 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-const userController = require('../controller/userController');
+const userController = require('../controller/userController.js');
 
 // Route to get all userName where role is Lead Owner
 router.get('/lead-owners', userController.getLeadOwners);
