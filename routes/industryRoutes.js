34f2e52eeb0c197b03c3e0accc53446172b56e48
Debayan Mondal <81500329+Debayanmondal@users.noreply.github.com>
2024-08-07 23:36:48 +0530

    Update industryRoutes.js

--- routes/industryRoutes.js
+++ routes/industryRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-const industryController = require('../controllers/industryController');
+const industryController = require('../controller/industryController');
 
 // Route to get all industryName
 router.get('/industries', industryController.getAllIndustries);
